902bdf925fc792290e21077dcfa730a3d255d172
jinwoong16 <baseball0020@gmail.com>
2024-06-30 19:34:39 +0900

    Initial Commit

---
+++ McspicyShanghaiDeluxe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  McspicyShanghaiDeluxe
+//
+//  Created by jinwoong Kim on 6/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
