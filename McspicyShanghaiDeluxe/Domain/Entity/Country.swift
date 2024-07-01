8e0bdcb798fa69948bf9d1d688d4319556c92e5d
jinwoong16 <baseball0020@gmail.com>
2024-07-01 14:25:41 +0900

    feat: configure `Country` object

---
+++ McspicyShanghaiDeluxe/Domain/Entity/Country.swift
@@ -0,0 +1,13 @@
+//
+//  Country.swift
+//  McspicyShanghaiDeluxe
+//
+//  Created by jinwoong Kim on 7/1/24.
+//
+
+import Foundation
+
+struct Country {
+    let name: String
+    let flag: String
+}
