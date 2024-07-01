2f605e3f6f1943922b2e2d6d60a484ae287b6757
jinwoong16 <baseball0020@gmail.com>
2024-07-01 14:09:49 +0900

    refactor: change parameter type in `BigmacCalculatable` protocol

--- McspicyShanghaiDeluxe/Domain/Protocols/BigmacCalculatable.swift
+++ McspicyShanghaiDeluxe/Domain/Protocols/BigmacCalculatable.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol BigmacCalculatable {
-    func exchange(_ money: String, to currency: Currency) -> String
+    func exchange(_ money: String, to currencyId: Currency.ID) -> String
     func countBigmacs(with dollarData: Double) -> Int
     func getAvailableCurrencies() -> [Currency]
 }
