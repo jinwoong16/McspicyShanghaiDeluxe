5cef3b4c8e6fbc0e0205d2169c85c4e1d4619304
jinwoong16 <baseball0020@gmail.com>
2024-07-02 21:06:33 +0900

    feat: define `Responsable` protocol

---
+++ McspicyShanghaiDeluxe/Network/Protocols/Responsable.swift
@@ -0,0 +1,12 @@
+//
+//  Responsable.swift
+//  McspicyShanghaiDeluxe
+//
+//  Created by jinwoong Kim on 7/2/24.
+//
+
+import Foundation
+
+protocol Responsable {
+    associatedtype Response: Decodable
+}
