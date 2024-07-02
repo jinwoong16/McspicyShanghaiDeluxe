e8b8e57614738df820e1f02c36ac74aac99f153c
jinwoong16 <baseball0020@gmail.com>
2024-07-02 20:25:27 +0900

    test: test `buildRequest` method of any `Requestable` object

---
+++ McspicyShanghaiDeluxeTests/RequestableTests/RequestableTests.swift
@@ -0,0 +1,43 @@
+//
+//  RequestableTests.swift
+//  McspicyShanghaiDeluxeTests
+//
+//  Created by jinwoong Kim on 7/2/24.
+//
+
+import XCTest
+@testable import McspicyShanghaiDeluxe
+
+final class RequestableTests: XCTestCase {
+    private var stub: (any Requestable)!
+
+    override func setUpWithError() throws {
+        stub = RequestableStub(
+            baseURL: "https://example.com",
+            path: "/latest",
+            queryItems: [
+                .init(name: "first", value: "firstValue"),
+                .init(name: "second", value: "secondValue")
+            ],
+            method: .GET
+        )
+    }
+
+    override func tearDownWithError() throws {
+        stub = nil
+    }
+    
+    func test_buildRequest() throws {
+        // given
+        
+        // when
+        let result = try stub.buildRequest()
+        
+        // then
+        XCTAssertEqual("GET", result.httpMethod)
+        XCTAssertEqual("/latest", result.url?.path())
+        XCTAssertEqual("first=firstValue&second=secondValue", result.url?.query())
+        XCTAssertNil(result.httpBody)
+        XCTAssert(result.allHTTPHeaderFields!.isEmpty)
+    }
+}
