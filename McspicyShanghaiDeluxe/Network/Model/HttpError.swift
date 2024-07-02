9afb91fd0c43c8d73fc5e8b1fe966416312a8662
jinwoong16 <baseball0020@gmail.com>
2024-07-02 19:49:48 +0900

    feat: configure `HttpError` which would be ocurred

---
+++ McspicyShanghaiDeluxe/Network/Model/HttpError.swift
@@ -0,0 +1,13 @@
+//
+//  HttpError.swift
+//  McspicyShanghaiDeluxe
+//
+//  Created by jinwoong Kim on 7/2/24.
+//
+
+import Foundation
+
+enum HttpError: Error {
+    case errorWith(code: Int, data: Data)
+    case badResponse
+}
