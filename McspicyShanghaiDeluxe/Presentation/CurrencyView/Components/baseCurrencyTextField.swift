53d8794d121e92d3132deb1275d1ad0308719c8d
YuleGlycerine <figurefig13@gmail.com>
2024-07-03 02:04:26 +0900

    Define initial UI elements

--- McspicyShanghaiDeluxe/Presentation/CurrencyView/Components/baseCurrencyTextField.swift
+++ McspicyShanghaiDeluxe/Presentation/CurrencyView/Components/baseCurrencyTextField.swift
@@ -6,3 +6,12 @@
 //
 
 import UIKit
+
+class baseCurrencyTextField {
+    // TODO: 피커뷰 버튼으로 변경
+    let baseCurrencyCountryLabel = UILabel()
+    let baseCurrencyTextField = UITextField()
+    let baseCurrencySuffixLabel = UIButton()
+    let exchangeIcon = UILabel()
+    let TextFieldmaxCharactres = 10
+}
