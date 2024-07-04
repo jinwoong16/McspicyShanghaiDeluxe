519c7e09cc1a9b9de7add9d05302281e99ef5d81
jinwoong16 <baseball0020@gmail.com>
2024-07-04 16:22:28 +0900

    feat: add all models information in model store

--- McspicyShanghaiDeluxe/Data/Store/ModelStore.swift
+++ McspicyShanghaiDeluxe/Data/Store/ModelStore.swift
@@ -11,16 +11,23 @@ protocol ModelStore<Model> {
     associatedtype Model: Identifiable
     
     func fetch(by id: Model.ID) -> Model?
+    func fetchAll() -> [Model]
 }
 
 final class AnyModelStore<Model: Identifiable>: ModelStore {
     private let models: [Model.ID: Model]
+    private let _models: [Model]
     
     init(models: [Model]) {
         self.models = models.groupingByID()
+        self._models = models
     }
     
     func fetch(by id: Model.ID) -> Model? {
         models[id]
+    }
+    
+    func fetchAll() -> [Model] {
+        _models
     }
 }
