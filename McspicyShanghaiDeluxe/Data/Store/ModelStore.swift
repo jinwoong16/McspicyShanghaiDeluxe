6ccfd67bc50a4f77cc178dc4278a0fc161444a8c
jinwoong16 <baseball0020@gmail.com>
2024-07-04 14:07:23 +0900

    feat: configure model store type

---
+++ McspicyShanghaiDeluxe/Data/Store/ModelStore.swift
@@ -0,0 +1,26 @@
+//
+//  ModelStore.swift
+//  McspicyShanghaiDeluxe
+//
+//  Created by jinwoong Kim on 7/4/24.
+//
+
+import Foundation
+
+protocol ModelStore {
+    associatedtype Model: Identifiable
+    
+    func fetch(by id: Model.ID) -> Model?
+}
+
+final class AnyModelStore<Model: Identifiable>: ModelStore {
+    private let models: [Model.ID: Model]
+    
+    init(models: [Model]) {
+        self.models = models.groupingByID()
+    }
+    
+    func fetch(by id: Model.ID) -> Model? {
+        models[id]
+    }
+}
