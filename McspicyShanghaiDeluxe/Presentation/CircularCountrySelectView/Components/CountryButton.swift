43dac1ffb242085ff590f3e674ebf42ea4381f40
jinwoong16 <baseball0020@gmail.com>
2024-07-08 01:50:15 +0900

    feat: add check mark on button

--- McspicyShanghaiDeluxe/Presentation/CircularCountrySelectView/Components/CountryButton.swift
+++ McspicyShanghaiDeluxe/Presentation/CircularCountrySelectView/Components/CountryButton.swift
@@ -15,7 +15,15 @@ final class CountryButton: UIButton {
         return countryLabel
     }()
     
-    private let country: Country
+    private lazy var checkImageView: UIImageView = {
+        let checkImageView = UIImageView()
+        checkImageView.image = UIImage(systemName: "checkmark")
+        checkImageView.alpha = 0
+        
+        return checkImageView
+    }()
+    
+    private(set) var country: Country
     
     init(country: Country) {
         self.country = country
@@ -31,25 +39,36 @@ final class CountryButton: UIButton {
     
     override func sizeToFit() {
         super.sizeToFit()
+        let width = countryLabel.bounds.width + checkImageView.bounds.width
+        let height = countryLabel.bounds.height + checkImageView.bounds.height
     
-        frame = CGRect(x: 0, y: 0, width: countryLabel.bounds.width, height: countryLabel.bounds.height)
+        frame = CGRect(x: 0, y: 0, width: width + 10, height: height)
     }
     
     private func configureUI() {
         addSubview(countryLabel)
+        addSubview(checkImageView)
         
         countryLabel.translatesAutoresizingMaskIntoConstraints = false
+        checkImageView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             countryLabel.topAnchor.constraint(equalTo: topAnchor),
             countryLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
-            countryLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
             countryLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            checkImageView.leadingAnchor.constraint(equalTo: countryLabel.trailingAnchor, constant: 10),
+            checkImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            checkImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
         ])
+        
+        checkImageView.transform = CGAffineTransform(translationX: -30, y: 0).scaledBy(x: 0.3, y: 0.3)
     }
     
     func configureLabel() {
         countryLabel.text = "\(country.name) / \(country.currency.code)"
         countryLabel.sizeToFit()
+        checkImageView.sizeToFit()
+    }
     }
 }
