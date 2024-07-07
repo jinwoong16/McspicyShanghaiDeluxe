a7e39fd04fa0ea81038b24e7870dd32d9ea3e8d9
YuleGlycerine <figurefig13@gmail.com>
2024-07-07 20:50:35 +0900

    feat: initialize ConvertedAmountLabel

--- McspicyShanghaiDeluxe/Presentation/CurrencyView/Components/ConvertedAmountLabel.swift
+++ McspicyShanghaiDeluxe/Presentation/CurrencyView/Components/ConvertedAmountLabel.swift
@@ -13,4 +13,30 @@ final class ConvertedAmountLabel: UIView {
     let convertedAmountSuffixLabel = UILabel()
     let fromLabel = UILabel() //에서
     let toLabel = UILabel() //(으)로
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        backgroundColor = .backgroundColor
+        
+        addSubview(destinationCountryButton)
+        addSubview(convertedAmountLabel)
+        addSubview(convertedAmountSuffixLabel)
+        addSubview(fromLabel)
+        addSubview(toLabel)
+        
+        destinationCountryButton.translatesAutoresizingMaskIntoConstraints = false
+        convertedAmountLabel.translatesAutoresizingMaskIntoConstraints = false
+        convertedAmountSuffixLabel.translatesAutoresizingMaskIntoConstraints = false
+        fromLabel.translatesAutoresizingMaskIntoConstraints = false
+        toLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
 }
