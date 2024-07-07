6ec1181fded31d825b65f3787a7a41b7b5285f84
jinwoong16 <baseball0020@gmail.com>
2024-07-07 16:56:30 +0900

    feat: add `CircularCountrySelectViewController` file

---
+++ McspicyShanghaiDeluxe/Presentation/CircularCountrySelectView/CircularCountrySelectViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CircularCountrySelectViewController.swift
+//  McspicyShanghaiDeluxe
+//
+//  Created by jinwoong Kim on 7/7/24.
+//
+
+import UIKit
+
+final class CircularCountrySelectViewController: UIViewController {
+    // MARK: - Components
+    
+    // MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
