a0eab024908517ced5282a3cf6f3a97a1c1b35c7
jinwoong16 <baseball0020@gmail.com>
2024-07-07 17:19:27 +0900

    feat: add `CountryButton` view

---
+++ McspicyShanghaiDeluxe/Presentation/CircularCountrySelectView/Components/CountryButton.swift
@@ -0,0 +1,49 @@
+//
+//  CountryButton.swift
+//  McspicyShanghaiDeluxe
+//
+//  Created by jinwoong Kim on 7/7/24.
+//
+
+import UIKit
+
+final class CountryButton: UIButton {
+    private lazy var countryLabel: UILabel = {
+        let countryLabel = UILabel()
+        countryLabel.font = .interLight(ofSize: 17)
+        
+        return countryLabel
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        addSubview(countryLabel)
+        
+        countryLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            countryLabel.topAnchor.constraint(equalTo: topAnchor),
+            countryLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            countryLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            countryLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+    
+    func setup(with country: Country) {
+        countryLabel.text = "\(country.name) / \(country.currency.code)"
+    }
+}
+
+#Preview {
+    let button = CountryButton()
+    button.setup(with: Country(isoCountryCode: "", name: "Korea", flag: "🇰🇷", currency: Currency(name: "won", code: "KRW", rate: 0), localPrice: 0))
+    return button
+}
