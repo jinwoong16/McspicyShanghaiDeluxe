8cc5c90e0187122841f43ec24b7800e3de78e39b
jinwoong16 <baseball0020@gmail.com>
2024-07-07 22:34:09 +0900

    feat: add `CircularCountrySelectViewController`

--- McspicyShanghaiDeluxe/Presentation/CircularCountrySelectView/CircularCountrySelectViewController.swift
+++ McspicyShanghaiDeluxe/Presentation/CircularCountrySelectView/CircularCountrySelectViewController.swift
@@ -9,9 +9,37 @@ import UIKit
 
 final class CircularCountrySelectViewController: UIViewController {
     // MARK: - Components
+    private lazy var circularContryButtonsView = CircularCountryButtonsView(
+        countries: countries
+    )
+    
+    private let countries: [Country]
+    
+    init(countries: [Country]) {
+        self.countries = countries
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        view.addSubview(circularContryButtonsView)
+        
+        circularContryButtonsView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            circularContryButtonsView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
+            circularContryButtonsView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor),
+            circularContryButtonsView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            circularContryButtonsView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
     }
 }
