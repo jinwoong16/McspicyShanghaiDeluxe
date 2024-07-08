a4b7b542a68fcf910fedce865ec8f84556390143
jinwoong16 <baseball0020@gmail.com>
2024-07-08 21:06:07 +0900

    refactor: separate delegate method

--- McspicyShanghaiDeluxe/Presentation/CurrencyView/Components/BaseCurrencyView.swift
+++ McspicyShanghaiDeluxe/Presentation/CurrencyView/Components/BaseCurrencyView.swift
@@ -89,7 +89,7 @@ final class BaseCurrencyCountryLabel: UILabel {
     }
 }
 
-final class BaseCurrencyTextField: UITextField, UITextFieldDelegate {
+final class BaseCurrencyTextField: UITextField {
     let textFieldmaxCharacters = 10
     private let placeholderAttributes: [NSAttributedString.Key: Any] = [
         .foregroundColor: UIColor.lightGray,
@@ -132,7 +132,9 @@ final class BaseCurrencyTextField: UITextField, UITextFieldDelegate {
             bottomBorder.frame = CGRect(x: 0, y: frame.height + 5, width: frame.width, height: 1)
         }
     }
-    
+}
+
+extension BaseCurrencyTextField: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         guard let currentText = textField.text as NSString? else { return true }
         let newText = currentText.replacingCharacters(in: range, with: string)
@@ -150,22 +152,6 @@ final class BaseCurrencyTextField: UITextField, UITextFieldDelegate {
             textField.text = ""
         }
         return false
-    }
-}
-
-extension String {
-    func addThousandSeparators() -> String? {
-        let numberFormatter = NumberFormatter()
-        numberFormatter.numberStyle = .decimal
-        numberFormatter.groupingSeparator = ","
-        numberFormatter.groupingSize = 3
-        
-        if let number = numberFormatter.number(from: self),
-           let formattedNumber = numberFormatter.string(from: number) {
-            return formattedNumber
-        } else {
-            return nil
-        }
     }
 }
 
